0d247ab1ce2df0c6d447c5be4a70a667ce341355
clebermaruiti <cleber.maruiti@gmail.com>
2016-03-03 23:48:44 -0300

    criação da conexão com o BD

--- p13/agenda/src/main/java/br/senac/tads/pi3/clebermaruiti/agenda/Agenda.java
+++ p13/agenda/src/main/java/br/senac/tads/pi3/clebermaruiti/agenda/Agenda.java
@@ -5,10 +5,30 @@
  */
 package br.senac.tads.pi3.clebermaruiti.agenda;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author clebermaruiti
  */
 public class Agenda {
-    //testando o commit...
+    public static Statement st; // Preparacao para conectar com o banco de dados (java.sql)
+    public static ResultSet rs; // Resultado da conexao com o banco de dados (java.sql)
+
+    private Connection obterConexao() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+
+        //Passo 1: Registrar drive JDBC
+        Class.forName("org.apache,derby.jdbc.ClientDataSouce");
+
+        //Passo 2: Abrir a conexão
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/agendabd");
+            
+        return conn;
+    }
 }
+
+
